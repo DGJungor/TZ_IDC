e35a4e9177e918daa940cace52eefd9332eadb76
ZhangJun <568171152@qq.com>
2017-12-20 10:14:49 +0800

    20171220  合并前推送

--- app/tools/cookie_session.php
+++ app/tools/cookie_session.php
@@ -8,8 +8,8 @@
     function byTokenGetUser($token) {
         $servername = "localhost";
         $username = "root";
-        $password = "";
-        $dbname = "new_idckx";
+        $password = "root";
+        $dbname = "idckx";
         $conn = new mysqli($servername, $username, $password,$dbname);
         if ($conn->connect_error) {
             // die("连接失败: " . $conn->connect_error);
