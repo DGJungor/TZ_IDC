ccf4be98ed5200561f4f48b460bf6f69fd44f7a8
ZhangJun <568171152@qq.com>
2018-01-10 13:28:45 +0800

    添加新的注册 验证模型  控制器

---
+++ app/web/controller/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+// +----------------------------------------------------------------------
+// | IDCKX
+// +----------------------------------------------------------------------
+// | Author:
+// +----------------------------------------------------------------------
+namespace app\web\controller;
+
+use cmf\controller\HomeBaseController;
+use think\Validate;
+use app\web\model\UserModel;
+
+class RegisterController extends HomeBaseController
+{
+
+	public function index()
+	{
+		//判断是否开放注册
+		$isOpenRegistration = cmf_is_open_registration();
+
+		//测试数据
+		dump($isOpenRegistration);
+	}
+
+	public function doRegister()
+	{
+		//判断是否接收到参数
+		if ($this->request->isPost()) {
+
+			//判断是否开放注册
+			$isOpenRegistration = cmf_is_open_registration();
+
+			//若未开放注册则抛出错误
+			if ($isOpenRegistration) {
+				$data = $this->request->post();
+
+
+
+			} else {
+				$this->error("网站未开放注册");
+			}
+
+		} else {
+			$this->error("请求错误");
+		}
+	}
+}
---
+++ app/web/validate/RegisterValidate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\ad\validate;
+
+use think\Validate;
+
+/**
+ * 前台注册验证类
+ *
+ * Class RegisterValidate
+ * @package app\ad\validate
+ *
+ */
+class RegisterValidate extends  Validate
+{
+
+}
