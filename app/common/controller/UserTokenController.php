cc63bc17d75ad05450a2ed8fd571055a4f777102
ZhangJun <568171152@qq.com>
2018-02-01 14:23:46 +0800

    添加公共模块

---
+++ app/common/controller/UserTokenController.php
@@ -0,0 +1,50 @@
+<?php
+// +----------------------------------------------------------------------
+// | IDCKX
+// +----------------------------------------------------------------------
+// | Author:
+// +----------------------------------------------------------------------
+namespace app\tools\controller;
+
+use cmf\controller\HomeBaseController;
+use think\Loader;
+use app\tools\model\UserTokenModel;
+
+class UserTokenController extends HomeBaseController
+{
+
+	/**
+	 *对token进行判断
+	 *
+	 * @author 张俊
+	 * @param null $token
+	 * @return array
+	 * @throws \think\db\exception\DataNotFoundException
+	 * @throws \think\db\exception\ModelNotFoundException
+	 * @throws \think\exception\DbException
+	 *
+	 */
+	function byTokenGetUser($token = null)
+	{
+		//实例化模型
+		$userTokenModel = new UserTokenModel();
+
+		//用token 获取相关的用户ID 以及相关信息
+		$result = $userTokenModel->getUserId($token);
+
+		//对token值进行判断
+		if (!is_null($result)) {
+
+			//判断是否过期
+			if (!$result["expire_time"] < time()) {
+				return ["userId" => $result["user_id"], "msg" => "获取成功"];
+			} else {
+				return ["userId" => -1, "msg" => "token已过期"];
+			}
+
+		} else {
+			return ["userId" => -1, "msg" => "token不存在"];
+		}
+	}
+
+}
