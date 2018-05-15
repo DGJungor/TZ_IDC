ba71122a8db3fd32abbc357750f42a7ad8fe7742
ZhangJun <568171152@qq.com>
2018-05-15 16:57:53 +0800

    添加  用户发送验证码 接口  和  验证码  模型

--- app/user/controller/VerificationCodeController.php
+++ app/user/controller/VerificationCodeController.php
@@ -18,193 +18,180 @@ class VerificationCodeController extends HomeBaseController
 {
 
 
-	/**
-	 * 发送邮箱验证码   TODO
-	 *
-	 * 通过发送随机码到 数据库(idckx_verification_code表中)  记录验证信息   验证时
-	 *
-	 * 参数:
-	 *      email:邮箱
-	 *
-	 * @author ZhangJun
-	 */
-	public function sendEmailCode()
-	{
+    /**
+     * 发送邮箱验证码   TODO
+     *
+     * 通过发送随机码到 数据库(idckx_verification_code表中)  记录验证信息   验证时
+     *
+     * 参数:
+     *      email:邮箱
+     *
+     * @author ZhangJun
+     */
+    public function sendEmailCode()
+    {
 
 
-		//获取参数
-		$data['email'] = $this->request->param('email');
+        //获取参数
+        $data['email'] = $this->request->param('email');
 
-		//实例验证类
-		$validate = new Validate([
-			'email' => 'require|email',
-		]);
+        //实例验证类
+        $validate = new Validate([
+            'email' => 'require|email',
+        ]);
 
-		//设置错误提示
-		$validate->message([
-			'email.require' => '邮箱不能为空!',
-			'email.email'   => '邮箱格式错误!',
-		]);
+        //设置错误提示
+        $validate->message([
+            'email.require' => '邮箱不能为空!',
+            'email.email' => '邮箱格式错误!',
+        ]);
 
-		//若验证不通过 则返回错I物提示
-		if (!$validate->check($data)) {
-			return idckx_ajax_echo(null, $validate->getError(), 0);
-		}
+        //若验证不通过 则返回错I物提示
+        if (!$validate->check($data)) {
+            return idckx_ajax_echo(null, $validate->getError(), 0);
+        }
 
-		//实例化模型
-		$verificationCodeModel = new VerificationCodeModel();
+        //实例化模型
+        $verificationCodeModel = new VerificationCodeModel();
 
-		$codeData = $verificationCodeModel->get(['account' => $data['email']]);
+        $codeData = $verificationCodeModel->get(['account' => $data['email']]);
 
-		//生成验证码
-		$code = mt_rand(1000, 9999);
+        //生成验证码
+        $code = mt_rand(1000, 9999);
 
-		//判断是否为空
-		if (empty($codeData)) {
-			//空
+        //判断数据库中是否存在此帐号的验证码
+        if (empty($codeData)) {
+            //空
 
-			//添加验证码到数据库中
-			$verificationCodeModel->addCode($data['email'], $code);
+            //添加验证码到数据库中
+            $verificationCodeModel->addCode($data['email'], $code);
 
-			//向邮箱发送验证码
-			cmf_send_email($data['email'], "【IDC快讯】", '您的验证码为:' . $code . '(24小时内有效)');
+            //向邮箱发送验证码
+            cmf_send_email($data['email'], "【IDC快讯】", '您的验证码为:' . $code . '(24小时内有效)');
 
-			return idckx_ajax_echo(null, "发送成功", 1);
+            return idckx_ajax_echo(null, "发送成功", 1);
 
-		} else {
-			//不为空
+        } else {
+            //不为空
 
-//			//发送时间超过24小时
-//			if ($codeData['send_time'] > strtotime('-1day')) {
-//
-//				$verificationCodeModel->updateCode('send_time', time());
-//
-//				$verificationCodeModel->save([
-//					'count'     => 'thinkphp',
-//					'send_time' => time()
-//				], ['account' => $data['email']]);
-//			}
+            if ($codeData['count'] > 9 && $codeData['send_time'] > strtotime('-1day')) {
+                //发送已经超过次数
 
-			$test = $verificationCodeModel->where('account', $data['email'])->find();
-
-//			dump();
-
-			if ($codeData['count'] > 9 && $codeData['send_time'] > strtotime('-1day')) {
-				//发送已经超过次数
-
-				return idckx_ajax_echo(null, '验证码发送超过10次,请24小时之后尝试', 0);
-
-			} else {
-				//重新发送验证码
-
-				return $verificationCodeModel->updateCode($data['email'], $code);
+                return idckx_ajax_echo(null, '验证码发送超过10次,请24小时之后尝试', 0);
 
 
-			}
+            } else {
+                //重新发送验证码
 
 
-			dump('不为空');
-		}
+                //如果发送次数  高于10次 并且  第一次发送时间 在24小时 之值 重置   发送时间数据
+                if ($codeData['send_time'] < strtotime('-1day')) {
+                    $verificationCodeModel->save([
+                        'send_time' => time(),
+                        'count' => 0
+                    ], ['account' => $data['email']]);
+                }
 
 
-		dump($codeData);
+                //更新验证码
+                $verificationCodeModel->updateCode($data['email'], $code);
+                //向邮箱发送验证码
+                cmf_send_email($data['email'], "【IDC快讯】", '您的验证码为:' . $code . '(24小时内有效)');
 
+                return idckx_ajax_echo(null, "发送成功", 1);
 
-		//清理过期验证码
-//		dump($verificationCodeModel->cleanExpireCode());
+            }
 
+        }
 
-		//打印数据
-//		dump($email);
+    }
 
-	}
-
-	/**
-	 * 手机发送验证码  TODO 必须功能  (延后开发)
-	 */
+    /**
+     * 手机发送验证码  TODO 必须功能  (延后开发)
+     */
 
 
 //=================================无用代码=========================================================================================
 
 
-	/**
-	 *
-	 */
-	public function send()
-	{
-		$validate = new Validate([
-			'username' => 'require',
-		]);
+    /**
+     *
+     */
+    public function send()
+    {
+        $validate = new Validate([
+            'username' => 'require',
+        ]);
 
-		$validate->message([
-			'username.require' => '请输入手机号或邮箱!',
-		]);
+        $validate->message([
+            'username.require' => '请输入手机号或邮箱!',
+        ]);
 
-		$data = $this->request->param();
-		if (!$validate->check($data)) {
-			$this->error($validate->getError());
-		}
+        $data = $this->request->param();
+        if (!$validate->check($data)) {
+            $this->error($validate->getError());
+        }
 
-		$accountType = '';
+        $accountType = '';
 
-		if (Validate::is($data['username'], 'email')) {
-			$accountType = 'email';
-		} else if (preg_match('/(^(13\d|15[^4\D]|17[013678]|18\d)\d{8})$/', $data['username'])) {
-			$accountType = 'mobile';
-		} else {
-			$this->error("请输入正确的手机或者邮箱格式!");
-		}
+        if (Validate::is($data['username'], 'email')) {
+            $accountType = 'email';
+        } else if (preg_match('/(^(13\d|15[^4\D]|17[013678]|18\d)\d{8})$/', $data['username'])) {
+            $accountType = 'mobile';
+        } else {
+            $this->error("请输入正确的手机或者邮箱格式!");
+        }
 
-		//TODO 限制 每个ip 的发送次数
+        //TODO 限制 每个ip 的发送次数
 
-		$code = cmf_get_verification_code($data['username']);
-		if (empty($code)) {
-			$this->error("验证码发送过多,请明天再试!");
-		}
+        $code = cmf_get_verification_code($data['username']);
+        if (empty($code)) {
+            $this->error("验证码发送过多,请明天再试!");
+        }
 
-		if ($accountType == 'email') {
+        if ($accountType == 'email') {
 
-			$emailTemplate = cmf_get_option('email_template_verification_code');
+            $emailTemplate = cmf_get_option('email_template_verification_code');
 
-			$user     = cmf_get_current_user();
-			$username = empty($user['user_nickname']) ? $user['user_login'] : $user['user_nickname'];
+            $user = cmf_get_current_user();
+            $username = empty($user['user_nickname']) ? $user['user_login'] : $user['user_nickname'];
 
-			$message = htmlspecialchars_decode($emailTemplate['template']);
-			$message = $this->display($message, ['code' => $code, 'username' => $username]);
-			$subject = empty($emailTemplate['subject']) ? 'ThinkCMF验证码' : $emailTemplate['subject'];
-			$result  = cmf_send_email($data['username'], $subject, $message);
+            $message = htmlspecialchars_decode($emailTemplate['template']);
+            $message = $this->display($message, ['code' => $code, 'username' => $username]);
+            $subject = empty($emailTemplate['subject']) ? 'ThinkCMF验证码' : $emailTemplate['subject'];
+            $result = cmf_send_email($data['username'], $subject, $message);
 
-			if (empty($result['error'])) {
-				cmf_verification_code_log($data['username'], $code);
-				$this->success("验证码已经发送成功!");
-			} else {
-				$this->error("邮箱验证码发送失败:" . $result['message']);
-			}
+            if (empty($result['error'])) {
+                cmf_verification_code_log($data['username'], $code);
+                $this->success("验证码已经发送成功!");
+            } else {
+                $this->error("邮箱验证码发送失败:" . $result['message']);
+            }
 
-		} else if ($accountType == 'mobile') {
+        } else if ($accountType == 'mobile') {
 
-			$param  = ['mobile' => $data['username'], 'code' => $code];
-			$result = hook_one("send_mobile_verification_code", $param);
+            $param = ['mobile' => $data['username'], 'code' => $code];
+            $result = hook_one("send_mobile_verification_code", $param);
 
-			if ($result !== false && !empty($result['error'])) {
-				$this->error($result['message']);
-			}
+            if ($result !== false && !empty($result['error'])) {
+                $this->error($result['message']);
+            }
 
-			if ($result === false) {
-				$this->error('未安装验证码发送插件,请联系管理员!');
-			}
+            if ($result === false) {
+                $this->error('未安装验证码发送插件,请联系管理员!');
+            }
 
-			cmf_verification_code_log($data['username'], $code);
+            cmf_verification_code_log($data['username'], $code);
 
-			if (!empty($result['message'])) {
-				$this->success($result['message']);
-			} else {
-				$this->success('验证码已经发送成功!');
-			}
+            if (!empty($result['message'])) {
+                $this->success($result['message']);
+            } else {
+                $this->success('验证码已经发送成功!');
+            }
 
-		}
+        }
 
 
-	}
+    }
 
 }
--- app/user/model/VerificationCodeModel.php
+++ app/user/model/VerificationCodeModel.php
@@ -10,101 +10,99 @@ class VerificationCodeModel extends Model
 {
 
 
-	/**
-	 *   查询验证码
-	 *
-	 */
-	public function queryCode($account)
-	{
+    /**
+     *   查询验证码
+     *
+     */
+    public function queryCode($account)
+    {
 
 //		$code = $this->
 
 
-	}
+    }
 
 
-	/**
-	 * 添加验证码
-	 *
-	 * 默认  1000 到 9999   4位数的随机验证码    默认过期时间   为一天后
-	 * @author ZhangJun
-	 */
-	public function addCode($account = null, $code = null)
-	{
-		$data = [
-			'account'     => $account,
-			'count'       => 1,
-			'send_time'   => time(),
-			'code'        => $code,
-			'expire_time' => strtotime('+1day'),
-		];
-		$res  = $this->save($data);
+    /**
+     * 添加验证码
+     *
+     * 默认  1000 到 9999   4位数的随机验证码    默认过期时间   为一天后
+     * @author ZhangJun
+     */
+    public function addCode($account = null, $code = null)
+    {
+        $data = [
+            'account' => $account,
+            'count' => 1,
+            'send_time' => time(),
+            'code' => $code,
+            'expire_time' => strtotime('+1day'),
+        ];
+        $res = $this->save($data);
 
-		return $res;
+        return $res;
 
-	}
-
-
-	/**
-	 * 更新验证码
-	 *
-	 * @author ZhangJun
-	 */
-	public function updateCode($account = null, $code = null)
-	{
-		$data = $this->where('account', $account)->find();
-
-		if ($data['send_time'] > strtotime('-1day')) {
+    }
 
 
-		}
+    /**
+     * 更新验证码
+     *
+     * @author ZhangJun
+     */
+    public function updateCode($account = null, $code = null)
+    {
 
-		$t = $this->where('account', $account)
-			->setInc('count');
+        //发送次数加一
+        $this->where('account', $account)
+            ->setInc('count');
 
-		return $t;
+        //更新验证码
+        $this->save([
+            'code' => $code,
+        ], ['account' => $account]);
 
-	}
-
-
-	/**
-	 * @param $userId
-	 * @param $token
-	 * @param int $expireTime
-	 * @param string $deviceType
-	 * @return int|string
-	 */
-	public function addUserTokenData($userId, $token, $expireTime = 3600, $deviceType = "web")
-	{
-		$data = [
-			"user_id"     => $userId,
-			"expire_time" => time() + $expireTime,
-			"create_time" => time(),
-			"token"       => $token,
-			"device_type" => $deviceType
-		];
-
-		$result = Db::name('user_token')->insert($data);
-
-		return $result;
-	}
+    }
 
 
-	/**
-	 * 清理所有过期code
-	 *
-	 * @author ZhangJun
-	 */
-	public function cleanExpireCode()
-	{
+    /**
+     * @param $userId
+     * @param $token
+     * @param int $expireTime
+     * @param string $deviceType
+     * @return int|string
+     */
+    public function addUserTokenData($userId, $token, $expireTime = 3600, $deviceType = "web")
+    {
+        $data = [
+            "user_id" => $userId,
+            "expire_time" => time() + $expireTime,
+            "create_time" => time(),
+            "token" => $token,
+            "device_type" => $deviceType
+        ];
 
-		$res = $this
-			->where('expire_time', '<', time())
-			->delete();
+        $result = Db::name('user_token')->insert($data);
 
-		return $res;
+        return $result;
+    }
 
 
-	}
+    /**
+     * 清理所有过期code
+     *
+     * @author ZhangJun
+     */
+    public function cleanExpireCode()
+    {
+
+        $res = $this
+            ->where('expire_time', '<', time())
+            ->delete();
+
+        return $res;
+
+
+    }
 
 }
